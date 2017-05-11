32e20a8991267ca543d9240997acbaa3dabbe81d
Frederik Kvartborg <fka@skemax.dk>
2017-05-12 00:00:00 +0200

    Add simple rules and test for transformSchema

--- test/transformSchema.js
+++ test/transformSchema.js
@@ -22,7 +22,9 @@ describe('Transform schema', () => {
       const transformedschema = transformSchema(schema)
       assertValidator(instance, transformedschema[instance])
     }
+  })
 
+  it('Should fallback to instanceof if no rules apply', () => {
     const { date } = transformSchema({
       date: Date
     })
@@ -30,7 +32,28 @@ describe('Transform schema', () => {
     expect(date(new Date())).to.be.true
 
     for (let type of ['string', 10, true, null, undefined, NaN, [], {}]) {
-      expect(date(type)).to.be.false
+      expect(() => date(type)).to.throw(Error)
     }
+  })
+
+  it('Should be able to parse nested arrays and objects', () => {
+    const schema = {
+      nestedArray: [{
+        number: String,
+        function: Function,
+        nestedArray: [String]
+      }],
+      nestedObject: {
+        string: String,
+        nestedObject: {
+          boolean: Boolean
+        }
+      }
+    }
+
+    const transformedSchema = transformSchema(schema)
+
+    expect(JSON.stringify(transformedSchema))
+      .to.be.equal(JSON.stringify(schema))
   })
 })
--- test/utils/types.js
+++ test/utils/types.js
@@ -1,6 +1,6 @@
 module.exports = {
-  string: ['string', '3141', 'true', 'false'],
-  number: [12031, 13513, 13.13, 0, 1, -3251],
+  string: ['string'],
+  number: [12031],
   boolean: [true, false],
   undefined: [undefined],
   null: [null],
